55e86e70f6a8b29c98c7845dd60e619589e81a10
Nathan Seva <thykof@protonmail.ch>
2023-02-01 17:50:04 +0100

    exit code

--- src/compiler.ts
+++ src/compiler.ts
@@ -33,45 +33,30 @@ function sortFiles(files: Array<string>): Array<string> {
   });
 }
 
-export async function compileDirectory(): Promise<boolean> {
-  const searchDirectory = (dir: string, fileList: string[] = []) => {
-    readdirSync(dir).forEach((file) => {
-      const filePath = join(dir, file);
-      if (statSync(filePath).isDirectory() && file !== "__tests__") {
-        fileList = searchDirectory(filePath, fileList);
-      } else if (filePath.endsWith(".ts")) {
-        fileList.push(filePath);
-      }
-    });
-    return fileList;
-  };
-
-  let files = searchDirectory("./assembly/contracts");
-  files = sortFiles(files);
-
-  return files.reduce(async (isOk, contract): Promise<boolean> => {
-    console.log(contract);
-    return Promise.resolve(
-      (await isOk) &&
-        (await compile([
-          "-o",
-          join("build", contract.replace(".ts", ".wasm")),
-          "-t",
-          join("build", contract.replace(".ts", ".wat")),
-          contract,
-        ]))
-    );
-  }, Promise.resolve(true));
-}
-
-export async function compileAll(): Promise<boolean> {
-  let files = readdirSync(dirToCompile).map((file) => {
-    return join(dirToCompile, file);
+function searchDirectory(dir: string, fileList: string[] = []): string[] {
+  readdirSync(dir).forEach((file) => {
+    const filePath = join(dir, file);
+    if (statSync(filePath).isDirectory() && file !== "__tests__") {
+      fileList = searchDirectory(filePath, fileList);
+    } else if (filePath.endsWith(".ts")) {
+      fileList.push(filePath);
+    }
   });
+  return fileList;
+};
+
+export async function compileAll(subdirectories: boolean): Promise<boolean> {
+  let files;
+  if (subdirectories) {
+    files = searchDirectory("./assembly/contracts");
+  } else {
+    files = readdirSync(dirToCompile).map((file) => {
+      return join(dirToCompile, file);
+    });
+  }
 
   // keep only files ending with `.ts`
   files = files.filter((file) => file.endsWith(".ts"));
-
   files = sortFiles(files);
 
   console.log(`${files.length} files to compile`);
@@ -98,11 +83,7 @@ export async function compileAll(): Promise<boolean> {
       () => {},
       async (argv) => {
         let result;
-        if (argv.subdirectories) {
-          result = await compileDirectory();
-        } else {
-          result = await compileAll();
-        }
+        result = await compileAll(argv.subdirectories as boolean);
         process.exit(result ? 0 : 1);
       }
     )
