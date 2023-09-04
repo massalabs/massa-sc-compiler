5ca86f591a05b145b9cab70a7a45a23debac07ab
Ben <br@massa.net>
2023-09-04 18:38:13 +0200

    Clean up compiler.ts

--- src/compiler.ts
+++ src/compiler.ts
@@ -2,30 +2,49 @@
 
 /* eslint-disable no-console */
 
-import { readFileSync, readdirSync, statSync } from 'fs';
+import { readdirSync, statSync } from 'fs';
 import { join, basename } from 'path';
 import asc from 'assemblyscript/dist/asc.js';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+import { readdir, readFile } from 'fs/promises';
 
 const dirToCompile = './assembly/contracts';
 
-async function compile(filePath: string) {
-  const { error, stdout, stderr } = await asc.main([
-    '-o',
-    join('build', basename(filePath.replace('.ts', '.wasm'))),
-    '-t',
-    join('build', basename(filePath.replace('.ts', '.wat'))),
-    filePath,
-  ]);
-  console.info('contract to compile ' + filePath);
-  if (error) {
-    console.log(stderr.toString());
-    throw new Error(`Error compiling contract ${filePath}: ${error.message}`);
+/**
+ * Compiles a file and logs the process and potential errors.
+ * @param filePath - The path of the file to compile.
+ */
+async function compile(filePath: string): Promise<void> {
+  try {
+    const { error, stdout, stderr } = await asc.main([
+      '-o',
+      join('build', basename(filePath.replace('.ts', '.wasm'))),
+      '-t',
+      join('build', basename(filePath.replace('.ts', '.wat'))),
+      filePath,
+    ]);
+    console.info('Contract to compile: ' + filePath);
+    if (error) {
+      console.error(stderr.toString());
+      throw new Error(`Error compiling contract ${filePath}: ${error.message}`);
+    }
+    console.info(stdout.toString());
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(`Failed to compile ${filePath}: ${error.message}`);
+    } else {
+      console.error(`Failed to compile ${filePath}: ${String(error)}`);
+    }
   }
-  console.log(stdout.toString());
 }
 
+/**
+ * Searches a directory recursively and returns a list of .ts files.
+ * @param dir - The directory to search.
+ * @param fileList - The list of files found so far.
+ * @returns A list of .ts files in the directory.
+ */
 function searchDirectory(dir: string, fileList: string[] = []): string[] {
   readdirSync(dir).forEach((file) => {
     const filePath = join(dir, file);
@@ -38,49 +57,97 @@ function searchDirectory(dir: string, fileList: string[] = []): string[] {
   return fileList;
 }
 
-export async function compileAll(subdirectories: boolean) {
+/**
+ * Gets a list of .ts files in a directory, optionally including subdirectories.
+ * @param dirToCompile - The directory to search.
+ * @param subdirectories - Whether to include subdirectories in the search.
+ * @returns A list of .ts files in the directory.
+ */
+async function getFiles(
+  dirToCompile: string,
+  subdirectories: boolean,
+): Promise<string[]> {
   let files: string[];
   if (subdirectories) {
     files = searchDirectory(dirToCompile);
   } else {
-    files = readdirSync(dirToCompile).map((file) => {
-      return join(dirToCompile, file);
-    });
+    files = (await readdir(dirToCompile)).map((file) =>
+      join(dirToCompile, file),
+    );
   }
 
-  // keep only files ending with `.ts`
-  files = files.filter((file) => file.endsWith('.ts'));
+  return files.filter((file) => file.endsWith('.ts'));
+}
 
-  console.log(`${files.length} files to compile`);
+/**
+ * Categorizes files based on whether they contain the string 'fileToByteArray('.
+ * @param files - The list of files to categorize.
+ * @returns A tuple containing two lists: files without the string and files with the string.
+ */
+async function categorizeFiles(files: string[]): Promise<[string[], string[]]> {
+  const filesWithByteArray: string[] = [];
+  const filesWithoutByteArray: string[] = [];
 
-  // first pass compilation with file NOT including "fileToByteArray"
   await Promise.all(
-    files
-      .filter(
-        (file) => !readFileSync(file, 'utf-8').includes('fileToByteArray('),
-      )
-      .map((file) => compile(file)),
+    files.map(async (file) => {
+      const content = await readFile(file, 'utf-8');
+      if (content.includes('fileToByteArray(')) {
+        filesWithByteArray.push(file);
+      } else {
+        filesWithoutByteArray.push(file);
+      }
+    }),
   );
 
-  // second pass with the rest of the files
-  await Promise.all(
-    files
-      .filter((file) =>
-        readFileSync(file, 'utf-8').includes('fileToByteArray('),
-      )
-      .map((file) => compile(file)),
-  );
+  return [filesWithoutByteArray, filesWithByteArray];
+}
+
+/**
+ * Compiles all files in a directory, optionally including subdirectories.
+ * @param dirToCompile - The directory containing the files to compile.
+ * @param subdirectories - Whether to include subdirectories in the compilation.
+ */
+async function compileAll(
+  dirToCompile: string,
+  subdirectories: boolean,
+): Promise<void> {
+  try {
+    const files = await getFiles(dirToCompile, subdirectories);
+    console.info(`${files.length} files to compile`);
+
+    const [filesWithoutByteArray, filesWithByteArray] = await categorizeFiles(
+      files,
+    );
+
+    await Promise.all(filesWithoutByteArray.map((file) => compile(file)));
+    await Promise.all(filesWithByteArray.map((file) => compile(file)));
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(`Failed to compile all files:  ${error.message}`);
+    } else {
+      console.error(`Failed to compile all files:  ${String(error)}`);
+    }
+  }
 }
 
 (async () => {
-  await yargs(hideBin(process.argv))
-    .command('*', 'Compile files in assembly/contracts', {}, async (argv) => {
-      await compileAll(argv.subdirectories as boolean);
-    })
-    .option('subdirectories', {
-      alias: 'r',
-      type: 'boolean',
-      description: 'Compile files in assembly/contracts and its subdirectories',
-    })
-    .parseAsync();
+  try {
+    await yargs(hideBin(process.argv))
+      .command('*', 'Compile files in assembly/contracts', {}, async (argv) => {
+        await compileAll(dirToCompile, argv.subdirectories as boolean);
+      })
+      .option('subdirectories', {
+        alias: 'r',
+        type: 'boolean',
+        description:
+          'Compile files in assembly/contracts and its subdirectories',
+      })
+      .parseAsync();
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(`Failed to parse arguments: ${error.message}`);
+    } else {
+      console.error(`Failed to parse arguments: ${String(error)}`);
+    }
+  }
 })();
