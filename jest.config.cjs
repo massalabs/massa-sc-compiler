0f4dc40bdf915daf4e1f0eadd59b8b8d7f0a4e68
Ben <br@massa.net>
2023-09-04 22:33:55 +0200

    Setup jest for typescript

---
+++ jest.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '<transform_regex>': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+};
---
+++ src/tests/compoler.spec.ts
@@ -0,0 +1,6 @@
+import * as test from '../compiler';
+describe('Test', () => {
+  it('should test', () => {
+    expect(true).toBe(true);
+  });
+});
